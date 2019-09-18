e0a7b59f14dff1771eb6dd90a48244d2b639d9b7
Quentin-D-NM <qdye@cnm.edu>
2019-09-18 16:56:19 -0600

    Added functionality to push operands to a stack.

--- src/edu/cnm/deepdive/Operator.java
+++ src/edu/cnm/deepdive/Operator.java
@@ -1,26 +1,28 @@
 package edu.cnm.deepdive;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Enumerated values representing operators in a postfix (RPN) calculator. Each operator has a token
- * that is used to recognize the operator in a string, and to represent the operator.
+ * that is used to recognize the operator in an input string, and to represent the operator in an
+ * output string.
  */
 public enum Operator {
 
-  /** Pop 2 values form stack, pushed sum of the 2 back onto stack*/
+  /** Pops 2 values from stack, pushes sum of the 2 back onto stack. */
   ADD("+"),
-  /** Pop 2 values form stack, pushed difference of the 2 back onto stack*/
+  /** Pops 2 values from stack, pushes difference of the 2 back onto stack. */
   SUBTRACT("-"),
-  /** Pop 2 values form stack, pushed product of the 2 back onto stack*/
+  /** Pops 2 values from stack, pushes product of the 2 back onto stack. */
   MULTIPLY("*"),
-  /** Pop 2 values form stack, pushed quotient (real) of the 2 back onto stack*/
+  /** Pops 2 values from stack, pushes quotient (real) of the 2 back onto stack. */
   DIVIDE("/"),
-  /** Pop 1 values form stack, pushed its square root back onto stack*/
+  /** Pops 1 value from stack, pushes its square root back onto stack. */
   SQUARE_ROOT("sqrt"),
-  /** Pop 2 values form stack, pushed the value f 1st raised to the second back onto stack*/
+  /** Pops 2 values from stack, pushes the value of 1st raised to the second back onto stack. */
   POWER("^"),
-  /** Pop 2 values form stack, pushed remainder (after truncated division) of the 2 back onto stack*/
+  /** Pops 2 values from stack, pushes remainder after truncated division of the 2 back onto stack. */
   MODULO("%");
 
   private String token;
@@ -29,18 +31,15 @@ public enum Operator {
     this.token = token;
   }
 
-  /**
-   * Exercises the Operator enum by printing all of the enumerated values (as an array).
-   * @param args command line arguments (ignored).
-   */
-  public static void main(String[] args) {
-    System.out.println(Arrays.toString(Operator.values()));
-  }
-
   @Override
   public String toString() {
     return token;
   }
 
-  // FIXME Add operate method w/ switch (later version will use @Override)
+  public static String tokenPattern() {
+    return "(?:^|\\s)(\\+|\\-|\\*|\\/|\\^|\\%|sqrt)(?:\\s|$)";
+  }
+
+  // TODO Add operate method w/ switch (later version will use @Override).
+
 }
